2ebfd01d9978ee852c64494dc12087be96536432
felipozito <felipozito@hotmail.com>
2022-04-21 11:18:57 -0500

    Mejorando Navbar

--- Ejercicios/NavbarPrueba/app.js
+++ Ejercicios/NavbarPrueba/app.js
@@ -1,9 +1,16 @@
-const btn = document.querySelector("label");
+const btn = document.querySelector("label i");
 const menu = document.querySelector(".list");
 const item = document.querySelectorAll(".item");
 
 btn.addEventListener("click", () => {
       menu.classList.toggle("show");
+      if (menu.classList.contains("show")) {
+            btn.classList.remove("bx-menu");
+            btn.classList.add("bx-x");
+      } else {
+            btn.classList.add("bx-menu");
+            btn.classList.remove("bx-x");
+      }
 });
 
 item.forEach((element) => {
