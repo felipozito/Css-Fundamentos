409fbc29c055df80500d39e2b273da6ed537e9e3
felipozito <felipozito@hotmail.com>
2022-06-21 12:46:39 -0500

    Modificando Carpetas

--- Ejercicios/Day2/animacion.js
+++
@@ -1,4 +0,0 @@
-let menu = document.querySelector("..menu");
-menu.addEventListener("click", () => {
-  menu.classList.contains("active") ? menu.classList.remove("no-animation") : menu.classList.add("active");
-});
